84dac837a92a20c2c0b3a0862e24658be708caef
Gee <glynester@hotmail.com>
2022-06-10 09:32:56 +0100

    Initial manual set up of project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Create a new component. This component should produce some html.
+
+const App=()=>{
+  return <div>Hi!</div>
+}
+
+// Take this component's generated html and put in on the page (in the DOM)
+ReactDOM.render(<App/>, document.querySelector('.container'));
+
+
+
+
