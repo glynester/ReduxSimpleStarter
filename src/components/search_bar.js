0f8c2a916966b78050f94de874f003690b595b3d
Gee <glynester@hotmail.com>
2022-06-11 20:42:31 +0100

    State for term and making it a controlled field

---
+++ src/components/search_bar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';  // Needed for React.createElement - when html is created.
+
+//  a class component is used whenever you want a component to have some type of internal record keeping, some ability for it to be aware of itself and what's happened to it since it's been rendered.
+//  We're going to enhance this thing's behavior by extending it with react based component class. Define a new class called search bar and give it access to all of the functionality that react.component has.
+// class SearchBar extends React.component{
+class SearchBar extends Component{
+  // This is how we define state or we initialize state in a class based component. Functional components do not have state; only class basic components do.
+  // The constructor function is the first and only function called automatically whenever a new instance of the class is created.
+  constructor(props){
+    super(props);
+    this.state={ term: '' }; // The only time that we ever manually change state liek this is inside of the constructor.
+  }
+  render(){
+    // return <input onChange={this.onInputChange} />;
+    // return <input onChange={event=>console.log(event.target.value)} />;
+    // We always manipulate our state with this.setState - we use this method to inform react that the state is changing
+    return (
+    <div>
+      <input 
+        value = {this.state.term} // Become a "Controlled" field - because field value comes from state
+        onChange={event=>this.setState({term: event.target.value})} />
+      {/* Value of the input: {this.state.term} */}
+    </div>
+    );
+  }
+
+  // onInputChange(event){
+  //   // console.log(event,event.target.value)
+  // }
+}
+// const SearchBar=()=>{
+//   return <input/>   // React.createElement
+// };
+
+export default SearchBar;
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+// require('dotenv').config();   // Required because I didn't use create-react-app to build this app. https://stackoverflow.com/questions/49579028/adding-an-env-file-to-react-project
+
+const YT_API_KEY=process.env.REACT_APP_YT_API_KEY;
+console.log("API_KEY=>",YT_API_KEY,process.env.REACT_APP_YT_API_KEY); // Don't enable!
 // Create a new component. This component should produce some html.
 
 const App=()=>{
-  return <div>Hi!</div>
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  )
+  
 }
 
 // Take this component's generated html and put in on the page (in the DOM)
