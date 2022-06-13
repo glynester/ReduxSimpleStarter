a7c41f127e0a786d9f5e669c9bb11a04ca38f16c
Gee <glynester@hotmail.com>
2022-06-13 09:10:46 +0100

    State set with list of videos

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-// require('dotenv').config();   // Required because I didn't use create-react-app to build this app. https://stackoverflow.com/questions/49579028/adding-an-env-file-to-react-project
+import REACT_APP_YT_API_KEY from '../.env';
 
-const YT_API_KEY=process.env.REACT_APP_YT_API_KEY;
-console.log("API_KEY=>",YT_API_KEY,process.env.REACT_APP_YT_API_KEY); // Don't enable!
-// Create a new component. This component should produce some html.
+// const YT_API_KEY=process.env.REACT_APP_YT_API_KEY;
+const YT_API_KEY=REACT_APP_YT_API_KEY;
+// console.log("API_KEY=>",YT_API_KEY,REACT_APP_YT_API_KEY); // Don't enable!
 
-const App=()=>{
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  )
-  
+// Data flow downstream flow means that only the most parent component in the application should be responsible for fetching data, e.g. from an API
+// refactor app component to a class based component so it can keep track of the list of videos on its state.
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={ videos:[] };
+
+    YTSearch( {key: REACT_APP_YT_API_KEY, term: 'surfboards'}, videos=>{
+      console.log(videos);
+      // this.setState({ videos: videos});
+      this.setState({ videos});
+      console.log('this.state',this.state);
+    });
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
-// Take this component's generated html and put in on the page (in the DOM)
 ReactDOM.render(<App/>, document.querySelector('.container'));
 
 
