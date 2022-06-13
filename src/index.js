4763ad5ead404f77b70c9988d431e7f8fc4ef64c
Gee <glynester@hotmail.com>
2022-06-13 22:35:32 +0100

    Individual video displaying

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import REACT_APP_YT_API_KEY from '../.env';  // Fudged because I can't get process.env.REACT_APP_YT_API_KEY to read the key.
 
 // const YT_API_KEY=process.env.REACT_APP_YT_API_KEY;
@@ -15,7 +16,7 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state={ videos:[] };
+    this.state={ videos:[], playing: "https://www.youtube.com/watch?v=Dvyuy9_1TgU" };
 
     YTSearch( {key: REACT_APP_YT_API_KEY, term: 'monkeys'}, videos=>{
       console.log(videos);
@@ -28,7 +29,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        {/* passing prop videos to video list */}
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={ this.state.videos } />
       </div>
     )
